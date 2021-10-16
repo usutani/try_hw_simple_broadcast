79780ac75bb8da50bcb9a3c64b390bd75d1488e3
Yasuhiro Usutani <y_usutani@mac.com>
2021-10-16 16:40:06 +0900

    メッセージ作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,20 @@ class MessagesController < ApplicationController
     @messages = Message.all
     @message = Message.new
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to messages_url
+    else
+      @messages = Message.all
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
 end
